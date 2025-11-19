3342535191702e0c490a9255da97ce1e8990b89d
Blake Lovasz <lovasz@umich.edu>
2025-11-18 22:02:44 -0500

    Finished set_up_database() function

--- marvel-rivals.py
+++ marvel-rivals.py
@@ -12,7 +12,22 @@ def get_api_key(filename):
     """
     with open(filename) as file:
         key = file.readlines()[2].split("=")[1].strip(" ")
+
     return key
 
 API_key = get_api_key("api_keys.txt")
 
+def set_up_database(name):
+    """
+    Arguement: str name
+
+    Return: tup (cur, conn)
+
+    Setting up a database using the name given.
+    Also setting up the cursor and connection objects.
+    """
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path + "/" + name)
+    cur = conn.cursor()
+
+    return (cur, conn)
