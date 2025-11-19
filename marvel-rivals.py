caf9519eeb4f4fa948e2b2b8285ee8dc1a96d384
Blake Lovasz <lovasz@umich.edu>
2025-11-18 22:53:15 -0500

    Stopped to ask for clarification on instructions.

--- marvel-rivals.py
+++ marvel-rivals.py
@@ -1,5 +1,6 @@
 import sqlite3
 import json
+import requests
 import os
 
 def get_api_key(filename):
@@ -17,6 +18,27 @@ def get_api_key(filename):
 
 API_key = get_api_key("api_keys.txt")
 
+def get_data(num):
+    """
+    Arguement: int num
+
+    Return: dict dic
+
+    Returning hero name, play time, and matches for num number of heroes.
+    """
+    hero_list_url = f"https://marvelrivalsapi.com/api/v1/heroes"
+    header = {"x-api-key": API_key}
+    r = requests.get(hero_list_url, headers = header)
+    try:
+        data = json.loads(r.text)[:num]
+    except:
+        print("Num is out of range")
+        data = json.loads(r.text)
+    
+    name_lst = []
+    for hero in data:
+        name_lst.append(hero["name"])
+
 def set_up_database(name):
     """
     Arguement: str name
@@ -31,3 +53,6 @@ def set_up_database(name):
     cur = conn.cursor()
 
     return (cur, conn)
+
+get_data(5)
+get_data(100)
