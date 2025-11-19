0a1a6b532354b0e173e51d41b77d9026ad0dcbd7
Blake Lovasz <lovasz@umich.edu>
2025-11-18 21:57:07 -0500

    importing necessary libraries, might need to add more

--- marvel-rivals.py
+++ marvel-rivals.py
@@ -1,6 +1,18 @@
+import sqlite3
+import json
+import os
+
 def get_api_key(filename):
+    """
+    Arguement: str filename
+
+    Return: str key
+
+    Taking a file of api_keys and getting back the Marvel Rivals API key 
+    """
     with open(filename) as file:
         key = file.readlines()[2].split("=")[1].strip(" ")
     return key
 
 API_key = get_api_key("api_keys.txt")
+
