6b6ebe64ae9e84d7053534c83c6d93978d02a233
Stanford Jusuf <sjusuf@umich.edu>
2025-12-11 15:49:33 -0500

    Fixed so only adds 25 unique rows everytime the file is run and updated the instructions

--- superhero.py
+++ superhero.py
@@ -4,12 +4,6 @@ import requests
 import os
 import re
 
-# Add these columns to the superhero table in superhero.db
-# mr_id INT
-# num_comics INT
-# first_issue INT
-
-
 def get_api_key(file_name, api_key_loc):
     """ 
     Arg: str file_name, int api_key_loc
@@ -31,47 +25,47 @@ def get_superhero_row(id):
     except:
         return (id, None, None, None, None, None, None)
     
-def get_superhero_rows(start_id, end_id):
+def get_superhero_rows(start_id, end_id, batch_size=25):
     """
     Arg: int start_id, int end_id
     Out: lst of tup of superhero id
-    Gets all the superhero row data from a specified start and end ID range
+    Gets superhero row data ensuring exactly `batch_size` unique rows
     """
     out_lst = []
-    counter = 0
+    seen_names = set()
     for id in range(start_id, end_id + 1):
-        if counter != 100:
-            unique_id, publisher, name, gender, origin, height, weight = get_superhero_row(id)
-            if publisher != "Marvel Comics":
-                continue 
-            else:
-                counter += 1
-                if gender == "Male":
-                    gender = 1
-                elif gender == "Female":
-                    gender = 2
-                else:
-                    gender = 3 # Not "Male" or "Female"
-                if re.search(r"Yugoslavia|Germany|Ireland|England|United\sKingdom|Russia|USSR|Latveria", origin):
-                    origin = 2 # "Europe"
-                elif re.search(r"Egypt|Africa", origin):
-                    origin = 3 # "Africa"
-                elif re.search(r"Vietnam", origin):
-                    origin = 4 # "Asia"
-                elif re.search(r"Eternity|Xandar|Arthros|Galaxy|Beyond|Attilan|Criminal|Armechadon|Dimension|Asgard", origin):
-                    origin = 6 # "Fictional Place"
-                elif re.search(r"unknown|Earth", origin):
-                    origin = 5 # "Somewhere in Earth"
-                elif re.search(r"-", origin):
-                    origin = 7 # "Unknown"
-                else:
-                    origin = 1 # "North America"
-                if re.search(r"\smeters", height):
-                    height = int(float(height.replace(" meters", ""))) * 100
-                elif re.search(r"\scm", height):
-                    height = int(height.replace(" cm", ""))
-                out_lst.append((unique_id, name, gender, origin, height, weight.replace(" kg", "")))
+        unique_id, publisher, name, gender, origin, height, weight = get_superhero_row(id)
+        if publisher != "Marvel Comics" or not name:
+            continue
+        if name in seen_names:
+            continue
+        seen_names.add(name)
+        if gender == "Male":
+            gender = 1
+        elif gender == "Female":
+            gender = 2
         else:
+            gender = 3
+        if re.search(r"Yugoslavia|Germany|Ireland|England|United\sKingdom|Russia|USSR|Latveria", origin):
+            origin = 2
+        elif re.search(r"Egypt|Africa", origin):
+            origin = 3
+        elif re.search(r"Vietnam", origin):
+            origin = 4
+        elif re.search(r"Eternity|Xandar|Arthros|Galaxy|Beyond|Attilan|Criminal|Armechadon|Dimension|Asgard", origin):
+            origin = 6
+        elif re.search(r"unknown|Earth", origin):
+            origin = 5
+        elif re.search(r"-", origin):
+            origin = 7
+        else:
+            origin = 1
+        if re.search(r"\smeters", height):
+            height = int(float(height.replace(" meters", ""))) * 100
+        elif re.search(r"\scm", height):
+            height = int(height.replace(" cm", ""))
+        out_lst.append((unique_id, name, gender, origin, height, weight.replace(" kg", "")))
+        if len(out_lst) == batch_size:
             break
     return out_lst
 
@@ -93,8 +87,8 @@ def setup_tables(conn, cur):
     cur.execute(
         """
         CREATE TABLE IF NOT EXISTS superheros
-        (id INTEGER,
-        name_id INTEGER PRIMARY KEY,
+        (id INTEGER PRIMARY KEY,        
+        name_id INTEGER,
         gender_id INTEGER,
         place_of_birth_id INTEGER,
         height INTEGER,
@@ -195,7 +189,14 @@ def setup_tables(conn, cur):
     )
     conn.commit()
 
-unique_names = {}
+def get_next_name_id(cur):
+    """
+    Arg: cur
+    Out: int result
+    """
+    cur.execute("SELECT MAX(name_id) FROM names")
+    result = cur.fetchone()[0]
+    return result if result else 0
 
 def add_rows_to_db_tables(conn, cur, batch):
     """
@@ -205,39 +206,29 @@ def add_rows_to_db_tables(conn, cur, batch):
     """
     for idx, row in enumerate(batch):
         unique_id, name, gender, origin, height, weight = row
-        if name not in unique_names.values():
-            name_id = len(unique_names) + 1
-            unique_names[name_id] = name
-            cur.execute(
-                "INSERT OR IGNORE INTO names (name_id, name) VALUES (?, ?)",
-                (name_id, name)
-            )
-            cur.execute(
-                """INSERT OR IGNORE INTO superheros
-                (id, name_id, gender_id, place_of_birth_id, height, weight)
-                VALUES (?, ?, ?, ?, ?, ?)""",
-                (unique_id, name_id, gender, origin, height, weight)
-            )
+        cur.execute("SELECT name_id FROM names WHERE name=?", (name,))
+        existing = cur.fetchone()
+        if existing:
+            name_id = existing[0]
         else:
-            name_id = [k for k, v in unique_names.items() if v == name][0]
-            cur.execute(
-                """INSERT OR IGNORE INTO superheros
-                (id, name_id, gender_id, place_of_birth_id, height, weight)
-                VALUES (?, ?, ?, ?, ?, ?)""",
-                (unique_id, name_id, gender, origin, height, weight)
-            )
+            name_id = get_next_name_id(cur) + 1
+            cur.execute("INSERT OR IGNORE INTO names (name_id, name) VALUES (?, ?)", (name_id, name))
+        cur.execute(
+            """INSERT OR IGNORE INTO superheros
+            (id, name_id, gender_id, place_of_birth_id, height, weight)
+            VALUES (?, ?, ?, ?, ?, ?)""",
+            (unique_id, name_id, gender, origin, height, weight)
+        )
     conn.commit()
 
-def chunked(iterable, size):
+def get_last_id(cur):
     """
-    Arg: lst iterable, int size
-    Out: none
-    Returns a list of data chunks of size 25
+    Arg: cur
+    Out: int result
     """
-    chunks = []
-    for i in range(0, len(iterable), size):
-        chunks.append(iterable[i:i+size])
-    return chunks
+    cur.execute("SELECT MAX(id) FROM superheros")
+    result = cur.fetchone()[0]
+    return result if result else 0
 
 def main():
     """
@@ -246,8 +237,9 @@ def main():
     """
     conn, cur = setup_db("superhero.db")
     setup_tables(conn, cur)
-    for batch in chunked(get_superhero_rows(1, 231), 25):
-        add_rows_to_db_tables(conn, cur, batch)
+    last_id = get_last_id(cur)
+    batch = get_superhero_rows(last_id + 1, 231, batch_size=25)
+    add_rows_to_db_tables(conn, cur, batch)
     conn.close()
 
 if __name__ == "__main__":
